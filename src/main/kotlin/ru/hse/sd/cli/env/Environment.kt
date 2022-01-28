3e43a05acc1ffbe8087cad1736895242601f00d5
Kirill <bitikit@gmail.com>
2022-01-28 19:35:52 +0300

    Implement environment

--- src/main/kotlin/ru/hse/sd/cli/env/Environment.kt
+++ src/main/kotlin/ru/hse/sd/cli/env/Environment.kt
@@ -3,15 +3,17 @@ package ru.hse.sd.cli.env
 import java.nio.file.Path
 
 class Environment {
+    private val variables = mutableMapOf<String, String>()
+
     val workingDirectory: Path
-        get() = TODO()
+        get() = Path.of("/").toAbsolutePath()
 
     fun resolvePath(path: Path): Path = if (path.isAbsolute) path else workingDirectory.resolve(path)
 
-    fun getVariable(name: String): String = TODO()
+    fun getVariable(name: String): String = variables.getOrDefault(name, "")
 
     fun putVariable(name: String, value: String) {
-        TODO()
+        variables[name] = value
     }
 
     operator fun get(variableName: String): String = getVariable(variableName)
