69bd48e74b08d15ca292628f0e04dfa555041a6b
peter <petyo333@gmail.com>
2022-01-28 14:03:48 +0300

    Add documentation and rename parser rule

--- src/main/kotlin/ru/hse/sd/cli/lang/Parser.kt
+++ src/main/kotlin/ru/hse/sd/cli/lang/Parser.kt
@@ -3,6 +3,12 @@ package ru.hse.sd.cli.lang
 import com.github.h0tk3y.betterParse.lexer.TokenMatchesSequence
 import ru.hse.sd.cli.command.Command
 
+/**
+ * Interface for the bash command parser
+ */
 interface Parser {
+    /**
+     * Build AST tree by [tokens]
+     */
     fun parse(tokens: TokenMatchesSequence): Command
 }
--- src/main/kotlin/ru/hse/sd/cli/lang/impl/CommandLexer.kt
+++ src/main/kotlin/ru/hse/sd/cli/lang/impl/CommandLexer.kt
@@ -3,7 +3,13 @@ package ru.hse.sd.cli.lang.impl
 import com.github.h0tk3y.betterParse.lexer.TokenMatchesSequence
 import ru.hse.sd.cli.lang.Lexer
 
+/**
+ * Implements a bash command lexer using [CommandGrammar]
+ */
 class CommandLexer : Lexer {
+    /**
+     * Implements a bash command tokenization using [CommandGrammar]
+     */
     override fun tokenize(string: String): TokenMatchesSequence = CommandGrammar.tokenizer.tokenize(string)
 }
 
