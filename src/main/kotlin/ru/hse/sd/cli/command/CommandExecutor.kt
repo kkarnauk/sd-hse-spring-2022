66add5855f9ec1343ff4f3363a1f6c14174c8f24
Kirill <bitikit@gmail.com>
2022-01-28 19:44:03 +0300

    Implement command executor

--- src/main/kotlin/ru/hse/sd/cli/command/CommandExecutor.kt
+++ src/main/kotlin/ru/hse/sd/cli/command/CommandExecutor.kt
@@ -1,5 +1,23 @@
 package ru.hse.sd.cli.command
 
-class CommandExecutor {
-    fun execute(command: String): CommandResult = TODO()
+import ru.hse.sd.cli.env.Environment
+import ru.hse.sd.cli.env.IoContext
+import ru.hse.sd.cli.lang.impl.CommandLexer
+import ru.hse.sd.cli.lang.impl.CommandParser
+import java.io.InputStream
+import java.io.OutputStream
+
+class CommandExecutor(
+    input: InputStream,
+    output: OutputStream,
+    error: OutputStream
+) {
+    private val environment = Environment()
+    private val context = IoContext(input, output, error)
+    private val lexer = CommandLexer()
+    private val parser = CommandParser()
+
+    private fun parse(command: String): Command = parser.parse(lexer.tokenize(command))
+
+    fun execute(command: String): CommandResult = parse(command).execute(context, environment)
 }
