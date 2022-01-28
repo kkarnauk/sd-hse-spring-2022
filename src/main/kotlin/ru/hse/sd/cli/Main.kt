fcaef0d13c786f63da9da95c7633c46f952769a9
Kirill <bitikit@gmail.com>
2022-01-28 20:37:41 +0300

    Implement main

--- src/main/kotlin/ru/hse/sd/cli/Main.kt
+++ src/main/kotlin/ru/hse/sd/cli/Main.kt
@@ -1,5 +1,22 @@
 package ru.hse.sd.cli
 
+import ru.hse.sd.cli.command.CommandExecutor
+import ru.hse.sd.cli.command.ExitResult
+
+private const val commandLinePrefix = ":> "
+
 fun main() {
-    TODO()
+    val executor = CommandExecutor(System.`in`, System.out, System.err)
+    while (true) {
+        print(commandLinePrefix)
+        val command = readLine()
+        if (command != null) {
+            val result = executor.execute(command)
+            if (result is ExitResult) {
+                break
+            }
+        } else {
+            println("Couldn't read a command.")
+        }
+    }
 }
