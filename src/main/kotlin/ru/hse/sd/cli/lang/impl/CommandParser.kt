06c0e5f3bf184f4c36420ca89894db72f7d4caff
Lebedev Egor <egorikl@list.ru>
2022-02-03 22:49:23 +0300

    Add EmptyCommand

--- src/main/kotlin/ru/hse/sd/cli/lang/impl/CommandParser.kt
+++ src/main/kotlin/ru/hse/sd/cli/lang/impl/CommandParser.kt
@@ -3,6 +3,7 @@ package ru.hse.sd.cli.lang.impl
 import com.github.h0tk3y.betterParse.lexer.TokenMatchesSequence
 import com.github.h0tk3y.betterParse.parser.parseToEnd
 import ru.hse.sd.cli.command.Command
+import ru.hse.sd.cli.command.EmptyCommand
 import ru.hse.sd.cli.lang.Parser
 
 /**
@@ -12,5 +13,6 @@ class CommandParser : Parser {
     /**
      * Implements a bash command parsing using [CommandGrammar]
      */
-    override fun parse(tokens: TokenMatchesSequence): Command = CommandGrammar.parseToEnd(tokens)
+    override fun parse(tokens: TokenMatchesSequence): Command =
+        if (tokens.all { it.type.ignored }) EmptyCommand else CommandGrammar.parseToEnd(tokens)
 }
