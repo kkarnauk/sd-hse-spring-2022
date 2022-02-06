e0447f3b012c4148c230998d2b9c0f961b938529
Худяков Юрий <catchitoo@mail.ru>
2022-02-06 23:55:52 +0500

    Add ExternalTests for Linux, macOS, Windows

---
+++ src/test/kotlin/ru/hse/sd/cli/executor/ExternalTest.kt
@@ -0,0 +1,28 @@
+package ru.hse.sd.cli.executor
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.EnabledOnOs
+import org.junit.jupiter.api.condition.OS
+
+abstract class ExternalTest : CommandExecutorTest() {
+    override fun withTestContext(block: TestContext.() -> Unit) = super.withTestContext {
+        closeInput()
+        block()
+    }
+}
+
+@EnabledOnOs(OS.LINUX, OS.MAC)
+class LinuxExternalTest : ExternalTest() {
+    @Test
+    fun `Simple external command test`() = withTestContext {
+        test("head -n 1 src/test/resources/lorem.txt", "Lorem ipsum dolor sit amet, consectetur")
+    }
+}
+
+@EnabledOnOs(OS.WINDOWS)
+class WindowsExternalTest : ExternalTest() {
+    @Test
+    fun `Simple external command test`() = withTestContext {
+        test("powershell -command \"Get-Content -Head 1 src/test/resources/lorem.txt\"", "Lorem ipsum dolor sit amet, consectetur")
+    }
+}
