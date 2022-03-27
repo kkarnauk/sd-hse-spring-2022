dafe995ae7a8ec4b284b89c3f9fb32091b5a67ab
Karnaukhov Kirill <kirill.karnaukhov@jetbrains.com>
2022-03-27 15:25:33 +0300

    Init project

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+import com.soywiz.korge.gradle.*
+
+buildscript {
+    val korgePluginVersion: String by project
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        google()
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+    }
+    dependencies {
+        classpath("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:$korgePluginVersion")
+    }
+}
+
+apply<KorgeGradlePlugin>()
+
+korge {
+    id = "ru.hse.sd.rogue"
+
+    targetJvm()
+    targetDesktop()
+}
