30047bc55a3596775f6aa923940439ab60d0b0af
Karnaukhov Kirill <kirill.karnaukhov@jetbrains.com>
2022-03-30 19:36:40 +0300

    Minor prototype character state

--- src/commonMain/kotlin/ru/hse/sd/rogue/game/state/character/CharacterState.kt
+++ src/commonMain/kotlin/ru/hse/sd/rogue/game/state/character/CharacterState.kt
@@ -1,7 +1,38 @@
 package ru.hse.sd.rogue.game.state.character
 
+import ru.hse.sd.rogue.game.controller.CharacterController
+import ru.hse.sd.rogue.game.logic.characteristics.Damage
+import ru.hse.sd.rogue.game.logic.characteristics.Health
+import ru.hse.sd.rogue.game.logic.item.Armor
+import ru.hse.sd.rogue.game.logic.item.Item
+import ru.hse.sd.rogue.game.logic.item.Potion
+import ru.hse.sd.rogue.game.logic.item.Weapon
+import ru.hse.sd.rogue.game.logic.position.Position
 import ru.hse.sd.rogue.game.state.State
 
-interface CharacterState : State {
+abstract class CharacterState(
+    val health: Health,
+    var position: Position,
+    protected var meleeDamage: Damage,
+    initItems: MutableList<Item> = mutableListOf()
+) : State {
+    protected val items: MutableList<Item> = initItems
+
+    protected val weapons: List<Weapon>
+        get() = items.filterIsInstance<Weapon>()
+
+    protected val armors: List<Armor>
+        get() = items.filterIsInstance<Armor>()
+
+    protected val potions: List<Potion>
+        get() = items.filterIsInstance<Potion>()
+
+    var currentWeapon: Weapon? = null
+
+    val damage: Damage
+        get() = currentWeapon?.damage ?: meleeDamage
+
+    protected abstract val controller: CharacterController
+
     // TODO
 }
--- src/commonMain/kotlin/ru/hse/sd/rogue/game/state/character/Mob.kt
+++ src/commonMain/kotlin/ru/hse/sd/rogue/game/state/character/Mob.kt
@@ -1,5 +1,16 @@
 package ru.hse.sd.rogue.game.state.character
 
-class Mob : CharacterState {
+import ru.hse.sd.rogue.game.controller.CharacterController
+import ru.hse.sd.rogue.game.logic.characteristics.Damage
+import ru.hse.sd.rogue.game.logic.characteristics.Health
+import ru.hse.sd.rogue.game.logic.position.Position
+
+class Mob(
+    health: Health,
+    position: Position,
+    meleeDamage: Damage
+) : CharacterState(health, position, meleeDamage) {
     // TODO
+    override val controller: CharacterController
+        get() = TODO("Not yet implemented")
 }
