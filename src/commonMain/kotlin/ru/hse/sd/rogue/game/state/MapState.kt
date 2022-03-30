c9cbf72ea038230826047a48fc4824c9c903350a
Karnaukhov Kirill <kirill.karnaukhov@jetbrains.com>
2022-03-30 19:35:40 +0300

    Store cells in map state

--- src/commonMain/kotlin/ru/hse/sd/rogue/game/state/MapState.kt
+++ src/commonMain/kotlin/ru/hse/sd/rogue/game/state/MapState.kt
@@ -1,5 +1,34 @@
 package ru.hse.sd.rogue.game.state
 
-class MapState : State {
+import ru.hse.sd.rogue.game.logic.position.Position
+
+class MapState(
+    initCells: List<List<CellState>>
+) : State {
+    private val cells: MutableList<MutableList<CellState>> = initCells.map { it.toMutableList() }.toMutableList()
+
+    init {
+        require(cells.isNotEmpty()) {
+            "Cannot construct an empty map."
+        }
+        cells.forEach { cell ->
+            require(cell.size == cells.first().size) {
+                "Map must be a rectangle."
+            }
+        }
+    }
+
+    operator fun get(x: Int, y: Int): CellState = cells[x][y]
+
+    operator fun set(x: Int, y: Int, cell: CellState) {
+        cells[x][y] = cell
+    }
+
+    operator fun get(position: Position): CellState = get(position.x, position.y)
+
+    operator fun set(position: Position, cell: CellState) {
+        set(position.x, position.y, cell)
+    }
+
     // TODO
 }
