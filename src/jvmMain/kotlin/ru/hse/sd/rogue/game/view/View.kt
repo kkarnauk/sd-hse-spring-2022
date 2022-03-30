c3a92ff700a6924907c94de07c639096589f9473
Karnaukhov Kirill <kirill.karnaukhov@jetbrains.com>
2022-03-30 22:42:08 +0300

    Make JVM only

--- src/jvmMain/kotlin/ru/hse/sd/rogue/game/view/View.kt
+++ src/jvmMain/kotlin/ru/hse/sd/rogue/game/view/View.kt
@@ -1,12 +1,13 @@
 package ru.hse.sd.rogue.game.view
 
 import ru.hse.sd.rogue.game.logic.action.Action
+import ru.hse.sd.rogue.game.logic.action.IrreversibleAction
 
 interface View {
     // TODO
 }
 
-class UpdateViewAction : Action {
+class UpdateViewAction : IrreversibleAction {
     override fun invoke(): Action.Result {
         TODO("Not yet implemented")
     }
