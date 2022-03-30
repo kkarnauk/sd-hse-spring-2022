5dda2053f3d88019a83fd24855727f7dd55f06c8
Karnaukhov Kirill <kirill.karnaukhov@jetbrains.com>
2022-03-30 23:53:53 +0300

    Implement registering of game check

--- src/jvmMain/kotlin/ru/hse/sd/rogue/game/state/GlobalState.kt
+++ src/jvmMain/kotlin/ru/hse/sd/rogue/game/state/GlobalState.kt
@@ -1,5 +1,23 @@
 package ru.hse.sd.rogue.game.state
 
-class GlobalState : State {
-    // TODO
+import ru.hse.sd.rogue.game.logic.action.ActionsManager
+import ru.hse.sd.rogue.game.logic.action.IrreversibleAction
+
+class GlobalState(
+    private val actionsManager: ActionsManager
+) : State {
+    init {
+        registerCheckGame()
+    }
+
+    private fun registerCheckGame() {
+        actionsManager.register(IrreversibleAction {
+            checkGame()
+            registerCheckGame()
+        })
+    }
+
+    private fun checkGame() {
+        println("Hello world")
+    }
 }
