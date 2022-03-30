adc839f439a86ded684f671d12d5a8e84ede306a
Karnaukhov Kirill <kirill.karnaukhov@jetbrains.com>
2022-03-31 01:07:19 +0300

    Implement input handler

--- src/jvmMain/kotlin/ru/hse/sd/rogue/game/logic/input/InputHandler.kt
+++ src/jvmMain/kotlin/ru/hse/sd/rogue/game/logic/input/InputHandler.kt
@@ -1,5 +1,26 @@
 package ru.hse.sd.rogue.game.logic.input
 
-class InputHandler {
-    // TODO
+import com.soywiz.korev.Key
+import com.soywiz.korge.input.keys
+import com.soywiz.korge.view.Stage
+import ru.hse.sd.rogue.game.controller.PlayerController
+import ru.hse.sd.rogue.game.logic.position.Direction
+
+class InputHandler(private val controller: PlayerController) {
+    fun Stage.mapKeys() {
+        keys {
+            down(Key.W) {
+                controller.move(Direction.Up)
+            }
+            down(Key.S) {
+                controller.move(Direction.Down)
+            }
+            down(Key.A) {
+                controller.move(Direction.Left)
+            }
+            down(Key.D) {
+                controller.move(Direction.Right)
+            }
+        }
+    }
 }
