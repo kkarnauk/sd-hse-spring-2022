03acc3ec7caefa716064117f398d572edfd4add1
Karnaukhov Kirill <kirill.karnaukhov@jetbrains.com>
2022-03-31 01:35:38 +0300

    Add player and boss to global state

--- src/jvmMain/kotlin/ru/hse/sd/rogue/game/state/GlobalState.kt
+++ src/jvmMain/kotlin/ru/hse/sd/rogue/game/state/GlobalState.kt
@@ -3,9 +3,13 @@ package ru.hse.sd.rogue.game.state
 import ru.hse.sd.rogue.game.logic.action.ActionPriority
 import ru.hse.sd.rogue.game.logic.action.ActionsManager
 import ru.hse.sd.rogue.game.logic.action.IrreversibleAction
+import ru.hse.sd.rogue.game.state.character.Player
+import ru.hse.sd.rogue.game.state.character.mob.BossMob
 
 class GlobalState(
-    private val actionsManager: ActionsManager
+    private val actionsManager: ActionsManager,
+    private val player: Player,
+    private val boss: BossMob
 ) : State {
     init {
         registerCheckGame()
@@ -19,6 +23,18 @@ class GlobalState(
     }
 
     private fun checkGame() {
-        println("Hello world")
+        if (!player.isAlive) {
+            loseGame()
+        } else if (!boss.isAlive) {
+            winGame()
+        }
+    }
+
+    private fun winGame() {
+        TODO()
+    }
+
+    private fun loseGame() {
+        TODO()
     }
 }
