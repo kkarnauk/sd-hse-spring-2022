18ff91970f975973c001e0e6d72089e03f2795e1
Karnaukhov Kirill <kirill.karnaukhov@jetbrains.com>
2022-03-31 01:35:22 +0300

    Add stubs of mobs

---
+++ src/jvmMain/kotlin/ru/hse/sd/rogue/game/state/character/mob/BossMob.kt
@@ -0,0 +1,9 @@
+package ru.hse.sd.rogue.game.state.character.mob
+
+import ru.hse.sd.rogue.game.logic.characteristics.Damage
+import ru.hse.sd.rogue.game.logic.characteristics.Health
+import ru.hse.sd.rogue.game.logic.position.Position
+
+class BossMob(health: Health, position: Position, meleeDamage: Damage) : Mob(health, position, meleeDamage) {
+    // TODO
+}
--- src/jvmMain/kotlin/ru/hse/sd/rogue/game/state/character/mob/Mob.kt
+++ src/jvmMain/kotlin/ru/hse/sd/rogue/game/state/character/mob/Mob.kt
@@ -1,11 +1,12 @@
-package ru.hse.sd.rogue.game.state.character
+package ru.hse.sd.rogue.game.state.character.mob
 
 import ru.hse.sd.rogue.game.controller.CharacterController
 import ru.hse.sd.rogue.game.logic.characteristics.Damage
 import ru.hse.sd.rogue.game.logic.characteristics.Health
 import ru.hse.sd.rogue.game.logic.position.Position
+import ru.hse.sd.rogue.game.state.character.CharacterState
 
-class Mob(
+open class Mob(
     health: Health,
     position: Position,
     meleeDamage: Damage
