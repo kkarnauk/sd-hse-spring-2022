90ee78bcf90a5bd0365743684b6752e035e72197
peter <petyo333@gmail.com>
2022-04-22 23:57:49 +0300

    Add more docs

--- src/jvmMain/kotlin/ru/hse/sd/rogue/game/logic/level/Level.kt
+++ src/jvmMain/kotlin/ru/hse/sd/rogue/game/logic/level/Level.kt
@@ -3,10 +3,16 @@ package ru.hse.sd.rogue.game.logic.level
 import ru.hse.sd.rogue.game.state.CellState
 import ru.hse.sd.rogue.game.state.character.CharacterState
 
-
+/**
+ * Game level
+ */
 data class Level(
+    /**
+     * Level cells
+     */
     val cells: List<List<CellState>>,
+    /**
+     * State of level characters
+     */
     val characters: List<CharacterState>
-) {
-    fun getBuilder() = LevelBuilder()
-}
+)
