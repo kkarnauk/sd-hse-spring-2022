ae3a9c92967a5b6b7fa61ac6d372cea63df3c982
peter <petyo333@gmail.com>
2022-04-22 23:47:56 +0300

    Add docs

--- src/jvmMain/kotlin/ru/hse/sd/rogue/game/logic/level/mobsfabric/MobsFabric.kt
+++ src/jvmMain/kotlin/ru/hse/sd/rogue/game/logic/level/mobsfabric/MobsFabric.kt
@@ -3,9 +3,25 @@ package ru.hse.sd.rogue.game.logic.level.mobsfabric
 import ru.hse.sd.rogue.game.logic.position.Position
 import ru.hse.sd.rogue.game.state.character.mob.MobState
 
+
+/**
+ * Generate mobs
+ */
 interface MobsFabric {
+    /**
+     * Generates some simple mob at [position]
+     */
     fun generateRandomEasyMob(position: Position): MobState
+    /**
+     * Generates some medium mob at [position]
+     */
     fun generateRandomMediumMob(position: Position): MobState
+    /**
+     * Generates some hard mob at [position]
+     */
     fun generateRandomHardMob(position: Position): MobState
+    /**
+     * Generates some boss mob at [position]
+     */
     fun generateBossMob(position: Position): MobState
 }
