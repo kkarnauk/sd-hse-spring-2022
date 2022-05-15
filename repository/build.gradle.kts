d5ea4c6d0abe735b3a55e8206fd92228a797ca57
Karnaukhov Kirill <kirill.karnaukhov@jetbrains.com>
2022-05-15 15:14:52 +0300

    Init repository-module

---
+++ repository/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "ru.hse.sd.hwproj.repo"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+    implementation(project(":model"))
+}
---
+++ repository/src/main/kotlin/ru/hse/sd/repo/Repository.kt
@@ -0,0 +1,23 @@
+package ru.hse.sd.repo
+
+import ru.hse.sd.hwproj.model.Checker
+import ru.hse.sd.hwproj.model.Homework
+import ru.hse.sd.hwproj.model.Submission
+
+interface Repository {
+    fun getHomework(id: Long): Homework.Content
+
+    fun getHomeworks(): List<Homework>
+
+    fun getSubmission(id: Long): Submission.Content
+
+    fun getSubmissionResult(submissionId: Long): Submission.Result
+
+    fun getSubmissions(homeworkId: Long): List<Submission>
+
+    fun addHomework(content: Homework.Content): Homework
+
+    fun addSubmission(content: Submission.Content): Submission
+
+    fun addChecker(content: Checker.Content): Checker
+}
