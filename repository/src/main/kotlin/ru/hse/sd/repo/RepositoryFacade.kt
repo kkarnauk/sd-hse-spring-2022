20254be934ad892ac03082b27365433dbbe439f1
Karnaukhov Kirill <kirill.karnaukhov@jetbrains.com>
2022-05-15 15:43:29 +0300

    Add stubs of repositories impls

---
+++ repository/src/main/kotlin/ru/hse/sd/repo/RepositoryFacade.kt
@@ -0,0 +1,39 @@
+package ru.hse.sd.repo
+
+import ru.hse.sd.hwproj.model.Checker
+import ru.hse.sd.hwproj.model.Homework
+import ru.hse.sd.hwproj.model.Submission
+
+class RepositoryFacade : Repository {
+    override fun getHomework(id: Long): Homework.Content {
+        TODO("Not yet implemented")
+    }
+
+    override fun getHomeworks(): List<Homework> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSubmission(id: Long): Submission.Content {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSubmissionResult(submissionId: Long): Submission.Result {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSubmissions(homeworkId: Long): List<Submission> {
+        TODO("Not yet implemented")
+    }
+
+    override fun addHomework(content: Homework.Content): Homework {
+        TODO("Not yet implemented")
+    }
+
+    override fun addSubmission(content: Submission.Content): Submission {
+        TODO("Not yet implemented")
+    }
+
+    override fun addChecker(content: Checker.Content): Checker {
+        TODO("Not yet implemented")
+    }
+}
