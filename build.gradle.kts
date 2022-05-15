067b64ff4750673ec79c25445768e4d00e1a6c0b
Karnaukhov Kirill <kirill.karnaukhov@jetbrains.com>
2022-05-15 14:42:46 +0300

    Init my-hw-proj

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "1.6.21"
+}
+
+group = "ru.hse.sd.hwproj"
+version = "1.0-SNAPSHOT"
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        testImplementation(kotlin("test"))
+    }
+}
