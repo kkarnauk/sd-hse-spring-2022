fe2c44273f1e555b80cebe82ae6b6dde22374abf
peter <petyo333@gmail.com>
2022-05-19 23:54:30 +0300

    Fronted for homework page

--- WebServer/src/main/kotlin/ru/hse/sd/webserver/ApiRequest.kt
+++ WebServer/src/main/kotlin/ru/hse/sd/webserver/ApiRequest.kt
@@ -7,7 +7,10 @@ import io.ktor.client.features.json.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import ru.hse.sd.hwproj.model.Homework
+import ru.hse.sd.hwproj.model.RunnerTask
+import ru.hse.sd.hwproj.model.Submission
 import java.net.URL
+import java.sql.Timestamp
 import kotlin.random.Random
 
 private val client = HttpClient(CIO) {
@@ -15,7 +18,6 @@ private val client = HttpClient(CIO) {
         serializer = GsonSerializer()
     }
 }
-
 
 private suspend inline fun <reified T> request(url: String, reqMethod: HttpMethod, reqBody: String? = null): T {
     return client.request(URL("http", apiHost, apiPort, url)) {
@@ -27,7 +29,6 @@ private suspend inline fun <reified T> request(url: String, reqMethod: HttpMetho
 }
 
 private val gson = GsonBuilder().create()
-
 
 // todo remove
 val hw = mutableListOf<Homework>()
@@ -48,4 +49,23 @@ suspend fun addHomework(homeworkContent: Homework.Content): Homework {
         )
     )
     return hw.last()
+}
+
+suspend fun getHomework(id: Long): Homework {
+//     TODO
+//    return request("/api/student/homework/${id}", HttpMethod.Get)
+    return hw.single { it.id == id }
+}
+
+suspend fun getSubmissions(id: Long): List<Submission> {
+//     TODO
+//    return request(TODO, HttpMethod.Get)
+    return List(Random.nextInt(7)) {
+        Submission(
+            it.toLong(), RunnerTask(0), Submission.Content(
+                Timestamp(Random.nextInt().toLong()),
+                URL("https://github.com/submission${Random.nextInt()}")
+            )
+        )
+    }
 }
