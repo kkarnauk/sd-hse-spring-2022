6570e761fe1fa35084522f2c04fa0139a4d17f3e
Karnaukhov Kirill <kirill.karnaukhov@jetbrains.com>
2022-05-20 23:49:31 +0300

    Implement facade for tasks queue

--- queue/src/main/kotlin/ru/hse/sd/hwproj/queue/TasksQueue.kt
+++ queue/src/main/kotlin/ru/hse/sd/hwproj/queue/TasksQueue.kt
@@ -3,5 +3,5 @@ package ru.hse.sd.hwproj.queue
 import ru.hse.sd.hwproj.model.RunnerTask
 
 interface TasksQueue {
-    fun sendRunnerTask(task: RunnerTask)
+    suspend fun sendRunnerTask(task: RunnerTask)
 }
